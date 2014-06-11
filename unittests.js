ae4da8c707f9a70b82c2e3b4b671519b1a3b0857
Shahab Layeghi <shahab.layeghi@intel.com>
2014-06-11 12:46:09 -0700

    added unittests.js file, so we can add tests

---
+++ unittests.js
@@ -0,0 +1,12 @@
+/*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global brackets, $, define, describe, it, xit, expect, beforeEach, afterEach, waits, waitsFor, waitsForDone, waitsForFail, runs, spyOn, jasmine, beforeFirst, afterLast */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    describe("LiveDev2", function () {
+        it("should pass", function () {
+            expect(true).toBe(true);
+        });
+    });
+});
